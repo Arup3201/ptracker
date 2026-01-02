a83965ca414d8d3330ecd9847e736fc86bdfb060
Arup3201 <arupjana7365@gmail.com>
2026-01-02 16:52:02 +0530

    fix: rate limiter allowing more requests than bucket size
    1. Fixed the issue where the rate limiter allows more requests than the
    size of the token bucket
    2. redis added to store access tokens to persist tokens even when server
       is restarted
    3. updated keycloak handler to store access tokens at redis
    4. authorize middleware updated with redis based access token
    5. docker compose redis service added
    6. updated main.go to support redis based access
    7. postgres update session method check error before returning

--- backend/handlers/middlewares.go
+++ backend/handlers/middlewares.go
@@ -10,7 +10,6 @@ import (
 	"net/http/httptest"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
@@ -105,11 +104,10 @@ func (fn HTTPErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var accessTokens = map[string]string{}
-
 type AuthMiddleware func(http.Handler) HTTPErrorHandler
 
-func Authorize(keycloakUrl, keycloakRealm string) AuthMiddleware {
+func Authorize(redis *redis.Client,
+	keycloakUrl, keycloakRealm string) AuthMiddleware {
 	return func(next http.Handler) HTTPErrorHandler {
 		return func(w http.ResponseWriter, r *http.Request) error {
 			public := []string{"/auth/login", "/auth/callback", "/auth/refresh"}
@@ -129,7 +127,16 @@ func Authorize(keycloakUrl, keycloakRealm string) AuthMiddleware {
 				}
 			}
 
-			sub, err := verifyAccessToken(keycloakUrl, keycloakRealm, accessTokens[sessionId])
+			accessToken, err := redis.Get(context.Background(), "access_token:session:"+sessionId).Result()
+			if err != nil {
+				return &HTTPError{
+					Code:    http.StatusUnauthorized,
+					Message: "User is not authorized",
+					Err:     fmt.Errorf("authorize: access token expired or missing"),
+				}
+			}
+
+			sub, err := verifyAccessToken(keycloakUrl, keycloakRealm, accessToken)
 			if err != nil {
 				return &HTTPError{
 					Code:    http.StatusUnauthorized,
@@ -173,7 +180,7 @@ type tokenBucket struct {
 	rate        int // refill rate per second
 	redisClient *redis.Client
 	tbFunc      *redis.Script
-	retry       int
+	retry       int64
 }
 
 const RedisLuaScript = `
@@ -187,30 +194,33 @@ local now_usec = tonumber(now[2])
 local now_ts = now_sec + now_usec / 1e6
 
 local tokens = tonumber(redis.call("HGET", key, "tokens"))
-local last_refill = tonumber(redis.call("HGET", key, "last_refill"))
-
 if tokens == nil then
-    tokens = capacity
-    last_refill = now_ts
+	tokens = capacity -- start full
+end
+
+local last_refill = tonumber(redis.call("HGET", key, "last_refill"))
+if last_refill == nil then
+	last_refill = now_ts
 end
 
 -- refill
 local elapsed = now_ts - last_refill
-local tokens_to_add = math.floor(elapsed * rate)
+local tokens_to_add = math.floor(elapsed) * rate
 
 if tokens_to_add > 0 then
 	tokens = math.min(capacity, tokens+tokens_to_add)
+	redis.call("HSET", key, "last_refill", now_ts)
 end
 
+-- deny if not enough tokens
 if tokens < 1 then
-    local retry_after = (1 - (elapsed*rate)) / rate
+    local retry_after = math.ceil(((1 - (elapsed*rate)) / rate)*1000)
     return {0, retry_after}
 end
 
-redis.call("HSET", key,
-	"tokens", tokens,
-	"last_refill", now_ts
-)
+-- consume
+tokens = tokens - 1
+redis.call("HSET", key, "tokens", tokens)
 
 return {1, 0}
 `
@@ -237,26 +247,17 @@ func (tb *tokenBucket) GetToken(key string) (string, error) {
 }
 
 func (tb *tokenBucket) AllowRequest(key string) (bool, error) {
-	_, err := tb.redisClient.HSetNX(tb.ctx, key, "tokens", tb.capacity).Result()
-	if err != nil {
-		return false, fmt.Errorf("allow request set tokens key: %w", err)
-	}
-	_, err = tb.redisClient.HSetNX(tb.ctx, key, "last_refill", time.Now()).Result()
-	if err != nil {
-		return false, fmt.Errorf("allow request set tokens key: %w", err)
-	}
-
 	value, err := tb.tbFunc.Run(tb.ctx, tb.redisClient, []string{key}, tb.capacity, tb.rate).Slice()
 	if err != nil {
 		return false, fmt.Errorf("allow request run lua script: %w", err)
 	}
 
-	if value[0] == 0 {
-		tb.retry = int(value[1].(float32) * 1000)
+	if value[0].(int64) == 0 {
+		tb.retry = value[1].(int64)
 		return false, nil
-	} else {
-		return true, nil
 	}
+
+	return true, nil
 }
 
 type RateLimiter func(HTTPErrorHandler) HTTPErrorHandler
@@ -265,14 +266,17 @@ func TokenBucketRateLimiter(rdc *redis.Client, capacity, rate int) RateLimiter {
 	bucket := CreateTokenBucket(rdc, capacity, rate)
 	return func(next HTTPErrorHandler) HTTPErrorHandler {
 		return func(w http.ResponseWriter, r *http.Request) error {
-			redisKey := "bucket:user:" + r.Context().Value("user_id").(string)
+			userId, err := utils.GetUserId(r)
+			if err != nil {
+				return fmt.Errorf("rate limiter get user id: %w", err)
+			}
+
+			redisKey := "bucket:user:" + userId
 			allow, err := bucket.AllowRequest(redisKey)
 			if err != nil {
-				// redis error, continue...
-				log.Printf("[ERROR] rate limiter: %s\n", err)
-				next.ServeHTTP(w, r)
-				return nil
+				return fmt.Errorf("rate limiter allow request: %w", err)
 			}
+
 			if allow {
 				rec := httptest.NewRecorder()
 
@@ -290,7 +294,7 @@ func TokenBucketRateLimiter(rdc *redis.Client, capacity, rate int) RateLimiter {
 				w.WriteHeader(rec.Result().StatusCode)
 				w.Write(rec.Body.Bytes())
 			} else {
-				w.Header().Add("X-Ratelimit-Retry-After", strconv.Itoa(int(bucket.retry)))
+				w.Header().Add("X-Ratelimit-Retry-After", strconv.Itoa(int(bucket.retry))+" ms")
 				w.WriteHeader(http.StatusTooManyRequests)
 			}
 			return nil
--- backend/main.go
+++ backend/main.go
@@ -91,8 +91,12 @@ func getEnvironment() error {
 	return nil
 }
 
-func attachMiddlewares(mux *http.ServeMux, pattern string, handler handlers.HTTPErrorHandler) {
-	authMiddleware := handlers.Authorize(KC_URL, KC_REALM)
+func attachMiddlewares(
+	mux *http.ServeMux,
+	redis *redis.Client,
+	pattern string,
+	handler handlers.HTTPErrorHandler) {
+	authMiddleware := handlers.Authorize(redis, KC_URL, KC_REALM)
 	mux.Handle(pattern, handlers.HTTPErrorHandler(authMiddleware(handler)))
 }
 
@@ -117,30 +121,32 @@ func main() {
 		log.Fatalf("[ERROR] server failed to migrate postgres: %s", err)
 	}
 
-	// handler
-	mux := http.NewServeMux()
-
-	kcHandler, err := handlers.CreateKeycloakHandler(
-		KC_URL, KC_REALM, KC_CLIENT_ID, KC_CLIENT_SECRET, KC_REDIRECT_URI,
-		HOME_URL, ENCRYPTION_SECRET,
-	)
-	if err != nil {
-		log.Fatalf("[ERROR] server failed to create keycloak handler: %s", err)
-	}
-
-	attachMiddlewares(mux, "GET /api/auth/login", kcHandler.KeycloakLogin)
-	attachMiddlewares(mux, "GET /api/auth/callback", kcHandler.KeycloakCallback)
-	attachMiddlewares(mux, "POST /api/auth/refresh", kcHandler.KeycloakRefresh)
-	attachMiddlewares(mux, "POST /api/auth/logout", kcHandler.KeycloakLogout)
-
+	// Redis
 	redis := redis.NewClient(&redis.Options{
 		Addr:     "127.0.0.1:6379",
 		Password: "",
 		DB:       0,
 		Protocol: 2,
 	})
+
+	// handler
+	mux := http.NewServeMux()
+
+	kcHandler, err := handlers.CreateKeycloakHandler(
+		KC_URL, KC_REALM, KC_CLIENT_ID, KC_CLIENT_SECRET, KC_REDIRECT_URI,
+		HOME_URL, ENCRYPTION_SECRET, redis,
+	)
+	if err != nil {
+		log.Fatalf("[ERROR] server failed to create keycloak handler: %s", err)
+	}
+
+	attachMiddlewares(mux, redis, "GET /api/auth/login", kcHandler.KeycloakLogin)
+	attachMiddlewares(mux, redis, "GET /api/auth/callback", kcHandler.KeycloakCallback)
+	attachMiddlewares(mux, redis, "POST /api/auth/refresh", kcHandler.KeycloakRefresh)
+	attachMiddlewares(mux, redis, "POST /api/auth/logout", kcHandler.KeycloakLogout)
+
 	rateLimiter := handlers.TokenBucketRateLimiter(redis, 5, 2)
-	attachMiddlewares(mux, "POST /api/projects", rateLimiter(handlers.CreateProject))
+	attachMiddlewares(mux, redis, "POST /api/projects", rateLimiter(handlers.CreateProject))
 
 	// server
 	server := &http.Server{
