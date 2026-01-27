e5cb1cbb3eea7e4368dde5504cb918112ca73418
Arup3201 <arupjana7365@gmail.com>
2026-01-27 18:09:36 +0530

    update db errors

--- backend/apierr/errors.go
+++ backend/apierr/errors.go
@@ -5,3 +5,4 @@ import "errors"
 var ErrResourceNotFound = errors.New("resource not found")
 var ErrDuplicate = errors.New("duplicate entry in the database")
 var ErrInvalidValue = errors.New("invalid value")
+var ErrForbidden = errors.New("forbidden action")
