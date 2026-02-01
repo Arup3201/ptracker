044987b33cac0f6b1c024a2e6843b1207ea77ed0
Arup3201 <arupjana7365@gmail.com>
2026-02-01 21:39:08 +0530

    fix: concret sql.DB instead of interface create transaction problems in WithTx

--- backend/internal/infra/database.go
+++ backend/internal/infra/database.go
@@ -1,19 +1,13 @@
 package infra
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 
 	_ "github.com/lib/pq"
-	"github.com/ptracker/internal/interfaces"
 )
 
-type database struct {
-	db *sql.DB
-}
-
-func NewDatabase(driver, dataSource string) (interfaces.Execer, error) {
+func NewDatabase(driver, dataSource string) (*sql.DB, error) {
 	var err error
 
 	if dataSource == "" {
@@ -31,19 +25,5 @@ func NewDatabase(driver, dataSource string) (interfaces.Execer, error) {
 		return nil, fmt.Errorf("ping: %w", err)
 	}
 
-	return &database{
-		db: db,
-	}, nil
-}
-
-func (d *database) QueryContext(ctx context.Context, stmt string, args ...any) (*sql.Rows, error) {
-	return d.db.QueryContext(ctx, stmt, args...)
-}
-
-func (d *database) QueryRowContext(ctx context.Context, stmt string, args ...any) *sql.Row {
-	return d.db.QueryRowContext(ctx, stmt, args...)
-}
-
-func (d *database) ExecContext(ctx context.Context, stmt string, args ...any) (sql.Result, error) {
-	return d.db.ExecContext(ctx, stmt, args...)
+	return db, nil
 }
