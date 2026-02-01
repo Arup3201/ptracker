a61e0ca81a7d8a1e2c8890acee2ffd9abcfe8121
Arup3201 <arupjana7365@gmail.com>
2026-02-01 21:40:07 +0530

    refactor: test cleanup before assertion

--- backend/internal/services/project_test.go
+++ backend/internal/services/project_test.go
@@ -10,7 +10,7 @@ import (
 func (suite *ServiceTestSuite) TestCreateProject() {
 	t := suite.T()
 
-	t.Run("should create project with non-empty id", func(t *testing.T) {
+	t.Run("should create project with non empty id", func(t *testing.T) {
 		sample_name := "Test Project"
 		sample_description := "Test Description"
 		sample_skills := "C++, Java"
@@ -23,10 +23,10 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 			USER_ONE,
 		)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().NotEqual("", id)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should create project with correct values", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -34,14 +34,13 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 		sample_skills := "C++, Java"
 
 		service := NewProjectService(suite.store)
-		id, err := service.CreateProject(suite.ctx,
+		id, _ := service.CreateProject(suite.ctx,
 			sample_name,
 			&sample_description,
 			&sample_skills,
 			USER_ONE,
 		)
 
-		suite.Require().NoError(err)
 		var p domain.Project
 		suite.db.QueryRowContext(suite.ctx,
 			"SELECT "+
@@ -50,26 +49,26 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 				"WHERE id=($1)",
 			id,
 		).Scan(&p.Id, &p.Name, &p.Description, &p.Skills, &p.Owner, &p.CreatedAt, &p.UpdatedAt)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(sample_name, p.Name)
 		suite.Require().Equal(sample_description, *p.Description)
 		suite.Require().Equal(sample_skills, *p.Skills)
 		suite.Require().Equal(USER_ONE, p.Owner)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should create project without description", func(t *testing.T) {
 		sample_name := "Test Project"
 		sample_skills := "C++, Java"
 
 		service := NewProjectService(suite.store)
-		id, err := service.CreateProject(suite.ctx,
+		id, _ := service.CreateProject(suite.ctx,
 			sample_name,
 			nil,
 			&sample_skills,
 			USER_ONE,
 		)
 
-		suite.Require().NoError(err)
 		var p domain.Project
 		suite.db.QueryRowContext(suite.ctx,
 			"SELECT "+
@@ -78,12 +77,13 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 				"WHERE id=($1)",
 			id,
 		).Scan(&p.Id, &p.Name, &p.Description, &p.Skills, &p.Owner, &p.CreatedAt, &p.UpdatedAt)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(sample_name, p.Name)
 		suite.Require().Equal((*string)(nil), p.Description)
 		suite.Require().Equal(sample_skills, *p.Skills)
 		suite.Require().Equal(USER_ONE, p.Owner)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should create project without skills", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -106,12 +106,13 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 				"WHERE id=($1)",
 			id,
 		).Scan(&p.Id, &p.Name, &p.Description, &p.Skills, &p.Owner, &p.CreatedAt, &p.UpdatedAt)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(sample_name, p.Name)
 		suite.Require().Equal(sample_description, *p.Description)
 		suite.Require().Equal((*string)(nil), p.Skills)
 		suite.Require().Equal(USER_ONE, p.Owner)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should fail to create project with empty name", func(t *testing.T) {
 		sample_name := ""
@@ -119,16 +120,16 @@ func (suite *ServiceTestSuite) TestCreateProject() {
 		sample_skills := "C++, Java"
 
 		service := NewProjectService(suite.store)
-		id, err := service.CreateProject(suite.ctx,
+		_, err := service.CreateProject(suite.ctx,
 			sample_name,
 			&sample_description,
 			&sample_skills,
 			USER_ONE,
 		)
 
-		suite.Require().EqualError(err, "invalid value")
+		suite.Cleanup()
 
-		service.store.Project().Delete(suite.ctx, id)
+		suite.Require().EqualError(err, "invalid value")
 	})
 }
 
@@ -149,10 +150,10 @@ func (suite *ServiceTestSuite) TestGetPrivateProject() {
 
 		project, err := service.GetPrivateProject(suite.ctx, id, USER_ONE)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().Equal(id, project.Id)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should get project with correct name, description and skills", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -168,12 +169,12 @@ func (suite *ServiceTestSuite) TestGetPrivateProject() {
 
 		project, err := service.GetPrivateProject(suite.ctx, id, USER_ONE)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().Equal(sample_name, project.Name)
 		suite.Require().Equal(sample_description, *project.Description)
 		suite.Require().Equal(sample_skills, *project.Skills)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should get project with role owner", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -189,10 +190,10 @@ func (suite *ServiceTestSuite) TestGetPrivateProject() {
 
 		project, err := service.GetPrivateProject(suite.ctx, id, USER_ONE)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().Equal(domain.ROLE_OWNER, project.Role)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should get project with correct owner", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -217,14 +218,15 @@ func (suite *ServiceTestSuite) TestGetPrivateProject() {
 				"WHERE id=($1)",
 			USER_ONE,
 		).Scan(&u.Id, &u.Username, &u.DisplayName, &u.Email, &u.AvatarURL, &u.IsActive, &u.CreatedAt, &u.UpdatedAt)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(u.Id, project.Owner.Id)
 		suite.Require().Equal(u.Username, project.Owner.Username)
 		suite.Require().Equal(u.DisplayName, project.Owner.DisplayName)
 		suite.Require().Equal(u.Email, project.Owner.Email)
 		suite.Require().Equal(u.AvatarURL, project.Owner.AvatarURL)
 		suite.Require().Equal(u.IsActive, project.Owner.IsActive)
-
-		service.store.Project().Delete(suite.ctx, id)
 	})
 	t.Run("should get forbidden error", func(t *testing.T) {
 		sample_name := "Test Project"
@@ -240,9 +242,9 @@ func (suite *ServiceTestSuite) TestGetPrivateProject() {
 
 		_, err = service.GetPrivateProject(suite.ctx, id, USER_TWO)
 
-		suite.Require().EqualError(err, "forbidden")
+		suite.Cleanup()
 
-		service.store.Project().Delete(suite.ctx, id)
+		suite.Require().EqualError(err, "forbidden")
 	})
 }
 
@@ -259,10 +261,10 @@ func (suite *ServiceTestSuite) TestGetMembers() {
 		service := NewProjectService(suite.store)
 		members, err := service.GetProjectMembers(suite.ctx, p, USER_TWO)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().Equal(1, len(members))
-
-		service.store.Project().Delete(suite.ctx, p)
 	})
 	t.Run("should get 2 members", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -275,10 +277,10 @@ func (suite *ServiceTestSuite) TestGetMembers() {
 		service := NewProjectService(suite.store)
 		members, err := service.GetProjectMembers(suite.ctx, p, USER_TWO)
 
+		suite.Cleanup()
+
 		suite.Require().NoError(err)
 		suite.Require().Equal(2, len(members))
-
-		service.store.Project().Delete(suite.ctx, p)
 	})
 	t.Run("should get forbidden error", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -290,9 +292,9 @@ func (suite *ServiceTestSuite) TestGetMembers() {
 		service := NewProjectService(suite.store)
 		_, err := service.GetProjectMembers(suite.ctx, p, USER_THREE)
 
-		suite.Require().EqualError(err, "forbidden")
+		suite.Cleanup()
 
-		service.store.Project().Delete(suite.ctx, p)
+		suite.Require().EqualError(err, "forbidden")
 	})
 }
 
@@ -308,6 +310,8 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 		service := NewProjectService(suite.store)
 
 		err := service.RespondToJoinRequests(suite.ctx, p, USER_ONE, USER_TWO, domain.JOIN_STATUS_ACCEPTED)
+
+		suite.Cleanup()
 
 		suite.Require().NoError(err)
 	})
@@ -329,6 +333,9 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&status)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(domain.JOIN_STATUS_ACCEPTED, status)
 	})
 	t.Run("should add role member after accepting join request", func(t *testing.T) {
@@ -349,9 +356,10 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&role)
-		suite.Require().Equal("Member", role)
 
 		suite.Cleanup()
+
+		suite.Require().Equal("Member", role)
 	})
 	t.Run("should reject join request", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -371,6 +379,9 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&status)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(domain.JOIN_STATUS_REJECTED, status)
 	})
 	t.Run("should reject join request without creating role", func(t *testing.T) {
@@ -390,9 +401,9 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan()
-		suite.Require().ErrorContains(err, "no rows in result set")
-
 		suite.Cleanup()
+
+		suite.Require().ErrorContains(err, "no rows in result set")
 	})
 	t.Run("should not transition join status from accepted to pending", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -404,6 +415,8 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 		service.RespondToJoinRequests(suite.ctx, p, USER_ONE, USER_TWO, domain.JOIN_STATUS_ACCEPTED)
 
 		err := service.RespondToJoinRequests(suite.ctx, p, USER_ONE, USER_TWO, domain.JOIN_STATUS_PENDING)
+
+		suite.Cleanup()
 
 		suite.Require().EqualError(err, "invalid value")
 	})
@@ -417,6 +430,8 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 		service.RespondToJoinRequests(suite.ctx, p, USER_ONE, USER_TWO, domain.JOIN_STATUS_REJECTED)
 
 		err := service.RespondToJoinRequests(suite.ctx, p, USER_ONE, USER_TWO, domain.JOIN_STATUS_ACCEPTED)
+
+		suite.Cleanup()
 
 		suite.Require().EqualError(err, "invalid value")
 	})
@@ -439,6 +454,9 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&status)
+
+		suite.Cleanup()
+
 		suite.Require().Equal(domain.JOIN_STATUS_PENDING, status)
 	})
 	t.Run("should not change join status from pending due to transaction error", func(t *testing.T) {
@@ -463,9 +481,10 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&status)
-		suite.Require().Equal(status, domain.JOIN_STATUS_PENDING)
 
 		suite.Cleanup()
+
+		suite.Require().Equal(status, domain.JOIN_STATUS_PENDING)
 	})
 	t.Run("should be forbidden to change the join status by non-member", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -476,6 +495,8 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 		service := NewProjectService(suite.store)
 
 		err := service.RespondToJoinRequests(suite.ctx, p, USER_THREE, USER_TWO, domain.JOIN_STATUS_ACCEPTED)
+
+		suite.Cleanup()
 
 		suite.Require().ErrorContains(err, "forbidden")
 	})
@@ -489,6 +510,8 @@ func (suite *ServiceTestSuite) TestRespondToJoinRequests() {
 		service := NewProjectService(suite.store)
 
 		err := service.RespondToJoinRequests(suite.ctx, p, USER_THREE, USER_TWO, domain.JOIN_STATUS_ACCEPTED)
+
+		suite.Cleanup()
 
 		suite.Require().ErrorContains(err, "forbidden")
 	})
--- backend/internal/services/public_test.go
+++ backend/internal/services/public_test.go
@@ -18,9 +18,9 @@ func (suite *ServiceTestSuite) TestJoinProject() {
 
 		err := service.JoinProject(suite.ctx, p, USER_TWO)
 
-		suite.Require().NoError(err)
-
 		suite.Cleanup()
+
+		suite.Require().NoError(err)
 	})
 	t.Run("should join project with status pending", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -40,9 +40,10 @@ func (suite *ServiceTestSuite) TestJoinProject() {
 				"WHERE project_id=($1) AND user_id=($2)",
 			p, USER_TWO,
 		).Scan(&status)
-		suite.Require().Equal("Pending", status)
 
 		suite.Cleanup()
+
+		suite.Require().Equal("Pending", status)
 	})
 	t.Run("should fail with duplicate value error", func(t *testing.T) {
 		p := suite.fixtures.Project(service_fixtures.ProjectParams{
@@ -53,6 +54,8 @@ func (suite *ServiceTestSuite) TestJoinProject() {
 		service.JoinProject(suite.ctx, p, USER_TWO)
 
 		err := service.JoinProject(suite.ctx, p, USER_TWO)
+
+		suite.Cleanup()
 
 		suite.Require().ErrorContains(err, "duplicate value")
 	})
